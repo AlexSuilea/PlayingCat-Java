9f3a246d4b2d6f35b272a0a5b0d273d87d198a4d
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-07-09 00:22:15 +0300

    Add files via upload

---
+++ PlayingCat/src/com/alexsuilea/Main.java
@@ -0,0 +1,8 @@
+package com.alexsuilea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(PlayingCat.isCatPlaying(true, 33));
+    }
+}
---
+++ PlayingCat/src/com/alexsuilea/PlayingCat.java
@@ -0,0 +1,9 @@
+package com.alexsuilea;
+
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if(temperature >=25 && temperature <= 35 && !summer)
+            return true;
+        else return temperature >= 25 && temperature <= 45 && summer;
+    }
+}
